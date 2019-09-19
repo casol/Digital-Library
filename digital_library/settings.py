ade28e8edfe568e313a8a7c1e009ff8a3523ad6b
casol <casoltys@gmail.com>
2019-09-19 20:57:08 +0200

    Update login template

--- digital_library/settings.py
+++ digital_library/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'widget_tweaks',
 
     # the social providers
     #'allauth.socialaccount.providers.facebook',
