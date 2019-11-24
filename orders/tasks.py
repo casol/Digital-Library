8a3d8993dbddf069da3dc23ac7877506e5076df1
casol <casoltys@gmail.com>
2019-11-24 23:51:59 +0100

    Task update

--- orders/tasks.py
+++ orders/tasks.py
@@ -1,6 +1,6 @@
 from celery import task
 from django.core.mail import send_mail
-from .models import Order
+from .models import Order, OrderBook
 
 
 @task
@@ -10,11 +10,15 @@ def order_created(order_id, user_email):
     has been successfully created.
     """
     order = Order.objects.get(id=order_id)
+    book_order = OrderBook.objects.filter(order=order_id)
+    books = ''
+    for book in book_order:
+        books += f'{book.book}, {book.quantity} x {book.price} USD \n'
     subject = f'Digital BookStore - order nr. {order.id}'
-    message = f'Hello {order.order_first_name},\n\nYou just have successfully placed an order.\n Find your order details below \
-    gfhuj'
+    message = f'Hello {order.order_first_name},\n\nYou just have successfully placed an order.\n Find\
+    your order details below \n {books} \nShipping details: '
     mail_sent = send_mail(subject,
                           message,
-                          'notification@digitalbookstorec.com',
+                          'notification@digitalbookstore.com',
                           [user_email])
     return mail_sent
